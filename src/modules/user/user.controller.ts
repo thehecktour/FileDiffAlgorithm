9ac2e132fcba655231a91d7812877b09d7d96f92
Atilio <atilio.jesus@grupoboticario.com.br>
2024-08-04 15:28:39 -0300

    feat: ✨ add basic structure for this project using nest.js

---
+++ src/modules/user/user.controller.ts
@@ -0,0 +1,13 @@
+import { Get, Controller } from "@nestjs/common";
+import { UserService } from "./user.service";
+
+@Controller()
+export class UserController{
+    constructor(private readonly userService: UserService){}
+
+    @Get()
+
+    getUserPassword(): string{
+        return this.userService.getUserPassword();
+    }
+}
